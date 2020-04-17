5a48453dd7415b49bead4d20a956488023f42937
owenburton <owenburton@users.noreply.github.com>
2020-04-17 09:45:48 -0700

    959 moves for a 3!

--- adv.py
+++ adv.py
@@ -4,11 +4,12 @@ from world import World
 
 from solution import dft
 
+import time
 import random
 from ast import literal_eval
 
 # Load world
-world = World()
+# world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
@@ -19,18 +20,34 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
-world.load_graph(room_graph)
+# room_graph=literal_eval(open(map_file, "r").read())
+# world.load_graph(room_graph)
 
 # Print an ASCII map
-world.print_rooms()
+# world.print_rooms()
 
-player = Player(world.starting_room)
+# player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-traversal_path = dft(player)
 # print(traversal_path)
+traversal_path = [0] * 1000
+start = time.time()
 
+while len(traversal_path) >= 960:
+    world = World()
+    room_graph=literal_eval(open(map_file, "r").read())
+    world.load_graph(room_graph)
+    player = Player(world.starting_room)
+    traversal_path = dft(player)
+print(f'time: {time.time() - start}')
+
+if len(traversal_path) < 960:
+    print(traversal_path)
+
+# got a 3 w/ randomization
+# time: 149.96372509002686
+# ['n', 's', 's', 'w', 'e', 'n', 'w', 'w', 's', 'w', 's', 's', 'n', 'n', 'e', 'n', 'e', 'n', 'w', 'w', 's', 'n', 'w', 's', 's', 's', 'w', 'w', 'w', 's', 'w', 'n', 's', 'e', 'n', 'e', 'e', 'n', 'w', 'w', 'w', 'e', 'e', 'e', 's', 'e', 's', 'w', 'w', 'e', 's', 'w', 'e', 'n', 'e', 's', 's', 's', 's', 'w', 's', 'w', 's', 's', 'w', 'e', 'n', 'w', 'e', 'n', 'e', 's', 's', 'n', 'n', 'n', 'e', 's', 's', 's', 's', 'w', 'e', 'n', 'e', 's', 'n', 'e', 's', 's', 'e', 'w', 's', 'w', 'e', 'n', 'n', 'n', 'w', 'w', 'n', 'n', 'n', 'n', 'n', 'w', 'w', 'w', 'w', 'w', 'w', 's', 'w', 's', 'n', 'e', 'n', 'e', 'e', 'n', 'w', 'n', 'n', 'w', 'n', 'w', 'e', 'e', 'n', 'w', 'n', 's', 'w', 'e', 'e', 'n', 's', 'e', 'e', 'e', 'n', 'w', 'w', 'e', 'e', 'e', 's', 'n', 'n', 'w', 'w', 'w', 'n', 'w', 'n', 'w', 'w', 'w', 'e', 'e', 'e', 'n', 's', 's', 'w', 'w', 'e', 's', 'n', 'e', 'e', 'n', 's', 's', 'w', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'n', 's', 'e', 'e', 'n', 'n', 'w', 'w', 'w', 'w', 'w', 'n', 'w', 'n', 'w', 'w', 'n', 'n', 'n', 's', 's', 'w', 'e', 's', 'w', 'w', 's', 'w', 'e', 'n', 'w', 'e', 'e', 'e', 'e', 'n', 'n', 's', 's', 'e', 'n', 'n', 'n', 'w', 'e', 'n', 'n', 's', 's', 's', 's', 's', 's', 'w', 'w', 'e', 'e', 'e', 'n', 'n', 'n', 'n', 's', 's', 's', 's', 's', 'e', 'n', 's', 'e', 'n', 'n', 'w', 'n', 's', 'e', 's', 's', 'e', 'e', 'n', 'w', 'n', 's', 'e', 'n', 'n', 'w', 'w', 'n', 'w', 'e', 's', 'e', 'n', 's', 'e', 's', 's', 's', 'e', 'n', 's', 'e', 'n', 'n', 'w', 'n', 'n', 'w', 'n', 'w', 'e', 'e', 'w', 's', 'e', 's', 's', 'e', 'n', 'e', 'e', 'w', 'w', 'n', 'e', 'e', 'n', 's', 'e', 'e', 'w', 'n', 'e', 'e', 'w', 'n', 'e', 'n', 's', 'e', 'n', 's', 'e', 'w', 'w', 'w', 's', 'w', 'n', 'n', 'n', 'n', 's', 's', 'e', 'n', 'e', 'n', 'n', 's', 'e', 's', 'n', 'e', 'w', 'w', 's', 'w', 'n', 's', 's', 'w', 's', 'w', 'n', 'n', 'n', 'n', 'e', 'e', 'w', 'w', 's', 'w', 'n', 'w', 'w', 'e', 'e', 's', 'w', 's', 's', 's', 'e', 'n', 'n', 's', 's', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'n', 's', 'e', 'n', 's', 'e', 's', 'w', 'e', 'n', 'e', 'n', 'w', 'n', 'n', 'w', 'e', 's', 's', 'e', 'n', 'n', 'n', 's', 's', 's', 's', 'e', 'e', 'n', 'w', 'n', 's', 'e', 'n', 's', 's', 'e', 's', 'e', 'w', 's', 's', 's', 's', 's', 's', 'n', 'w', 's', 'w', 'e', 's', 'e', 's', 's', 's', 'e', 's', 'n', 'w', 's', 's', 's', 's', 's', 's', 'n', 'e', 's', 's', 'w', 's', 's', 'n', 'n', 'e', 's', 'e', 's', 'e', 'e', 's', 's', 'n', 'n', 'w', 's', 's', 'n', 'n', 'w', 's', 's', 'n', 'n', 'n', 'e', 'e', 'n', 'e', 'e', 's', 's', 's', 's', 'n', 'n', 'e', 'w', 'n', 'n', 'e', 'w', 'w', 's', 's', 'n', 'n', 'w', 's', 'w', 'w', 'w', 's', 's', 'w', 's', 'n', 'e', 's', 's', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'e', 's', 's', 'n', 'n', 'e', 's', 'e', 'n', 'e', 's', 's', 'n', 'n', 'w', 's', 'w', 's', 'e', 's', 's', 'e', 'e', 'e', 'e', 'w', 'w', 'w', 'w', 'n', 'e', 'w', 'n', 'w', 's', 's', 's', 'e', 'w', 'n', 'n', 'n', 'n', 'n', 'w', 'w', 'n', 'w', 'w', 's', 'w', 'e', 's', 'w', 'w', 's', 'n', 'e', 'e', 's', 'w', 'e', 's', 'w', 'e', 's', 'w', 's', 's', 'n', 'e', 's', 's', 's', 'n', 'n', 'n', 'w', 'w', 's', 'n', 'e', 'n', 'w', 'n', 's', 'e', 'e', 'e', 's', 's', 's', 's', 's', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'w', 'w', 'e', 'e', 'e', 'n', 'n', 'e', 'w', 'n', 'e', 'n', 'e', 'n', 'e', 'n', 'n', 'n', 'n', 's', 's', 's', 'e', 'n', 'n', 'n', 's', 'e', 'n', 'n', 'e', 'e', 'n', 's', 'e', 'e', 'w', 'n', 's', 'w', 'w', 'n', 's', 'w', 's', 's', 'w', 's', 's', 'w', 's', 'w', 's', 'w', 'n', 'n', 'n', 'n', 's', 's', 'e', 'n', 'n', 's', 's', 'w', 's', 's', 's', 'e', 's', 'e', 's', 's', 'e', 'w', 'n', 'e', 'e', 'e', 'e', 'e', 'w', 'w', 'w', 's', 'e', 'e', 's', 'n', 'e', 'w', 'w', 'w', 's', 's', 'e', 'n', 's', 'e', 'w', 'w', 's', 'e', 'e', 'w', 'w', 's', 'e', 'e', 'w', 'w', 'n', 'n', 'n', 'n', 'n', 'w', 'w', 'n', 'w', 's', 's', 'n', 'n', 'n', 'e', 'e', 's', 'n', 'e', 's', 'e', 'e', 'e', 'n', 's', 'e', 'w', 'w', 'w', 'w', 'n', 'e', 'e', 'w', 'w', 'w', 'w', 'n', 'e', 'e', 'e', 'e', 'e', 'e', 's', 'n', 'w', 'w', 'w', 'w', 'w', 'n', 'e', 'n', 'n', 'e', 'n', 'n', 'e', 'e', 'e', 's', 'n', 'w', 'w', 'w', 's', 's', 'e', 'n', 'e', 'w', 's', 'w', 'w', 's', 'e', 's', 'n', 'e', 's', 'e', 'e', 'e', 's', 'n', 'w', 'w', 'w', 'n', 'e', 'n', 'e', 'w', 's', 'e', 'w', 'w', 'w', 'w', 's', 'w', 's', 'w', 's', 'w', 'w', 'w', 'n', 'w', 'w', 'w', 'w', 'n', 's', 'w', 'n', 's', 'w', 'n', 'w', 'w', 'e', 'n', 'w', 'e', 's', 'e', 's', 's', 'w', 's', 'w', 'w', 'w', 's', 'w', 's', 'e', 's', 's', 'w', 'n', 'w', 'e', 's', 'e', 'e', 's', 'e', 'n', 's', 'e', 'e', 's', 'w', 'w', 'w', 'w', 'e', 's', 'w', 'w', 'e', 'e', 's', 'w', 'w', 'w', 'w', 'e', 'e', 'e', 's', 'w', 'e', 's', 'w', 'e', 's', 's', 'n', 'w', 'w', 'e', 'e', 'n', 'n', 'n', 'e', 's', 's', 's', 's', 'e', 'w', 'n', 'e', 'w', 'n', 'n', 'n', 'n', 'n', 'e', 'e', 's', 'w', 's']
+# TESTS PASSED: 959 moves, 500 rooms visited
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -39,7 +56,6 @@ visited_rooms.add(player.current_room)
 
 for move in traversal_path:
     player.travel(move)
-    # player.travel(move[0]) # because traversal_path is a list of tuples
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
--- solution.py
+++ solution.py
@@ -1,68 +1,5 @@
-#### PLAN
-# start exploring (dfs)
-# keep track of your path
-# once you get to a room w/ no other ways to go or with no more unexplored routes
-# retrace your steps to the last room w unexplored routes
-# add those retrace steps to your path
-# start exploring again
-
-# def get_rooms(visited, room):
-#     lis = {room.get_room_in_direction(d):d for d in room.get_exits()}
-#     return lis
-
-
-# def opp_way(direction_string):
-#     opposite = {'n':'s', 's':'n', 'e':'w', 'w':'e'}
-#     return opposite[direction_string]
-
-
-# def answer(a_world, a_player, num_rooms):
-#     start = a_player.current_room
-#     explored = set()
-#     s = [[('start', start)]]
-#     route = []
-
-#     while len(s)>0:
-#         path = s.pop()
-#         room = path[-1][1]
-#         print(s)
-#         print(room)
-#         if room not in explored: 
-#             explored.add(room)
-
-#             ways_to_go = next_ways_and_rooms(explored, room)
-
-#             # record your new directions if there's nowhere to go
-#             if not ways_to_go:
-#                 # check if there's no more rooms to explore
-#                 if len(s)==0:
-#                     break
-#                 # if you have started recording the map
-#                 if len(route)>0:
-#                     print('route greater than zero')
-#                     last_checkpoint = s.pop()
-#                     s.append(last_checkpoint)
-#                     num_new_steps = len(path) - (len(last_checkpoint) - 1) # minus 1 bc last_checkpoint includes next room to be explored
-#                     new_path = path[-(num_new_steps):]
-#                     new_steps = [d for d in new_path]
-#                     retraced_steps = [opp_way(d) for d in new_steps]
-#                     route = route + new_steps + retraced_steps
-#                 # if you haven't started the map yet
-#                 else:
-#                     print('filling empty route')
-#                     route = [tup[0] for tup in path]
-            
-#             # if there are ways to go, then add those paths to the stack
-#             else:
-#                 for tup in ways_to_go:
-#                     # print(tup)
-#                     # print(route)
-#                     explored.add(tup[1])
-#                     s.append(path + [tup])
-#                     print(f'after: {s}')
-#     print(route[1:])
-#     return route[1:] # don't include 'start'
 from collections import deque
+from random import shuffle
 
 def get_rooms(room):
     lis = {room.get_room_in_direction(d):d for d in room.get_exits()}
@@ -102,6 +39,8 @@ def dft(a_player):
                     visited.add(tup[1])
 
             new_rooms = [(d, v.get_room_in_direction(d)) for d in v.get_exits()]
+            if new_rooms:
+                shuffle(new_rooms)
             for tup in new_rooms:
                 stack.append(path + [tup])
 
